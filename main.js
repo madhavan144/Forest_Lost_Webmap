f53ec2f92c6552e9c139c903cf483efb13a4e3e8
madhavan144 <madhavanm0809@gmail.com>
2025-07-17 17:42:55 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,48 @@
 let map = L.map("map").setView([7.8731, 80.7718], 7);
+function drawChart(data) {
+    const chartData = Object.entries(data).sort((a, b) => b[1] - a[1]); // Sort descending
+
+    const svg = d3.select("#chart")
+        .append("svg")
+        .attr("width", 600)
+        .attr("height", chartData.length * 20 + 50);
+
+    const margin = { top: 20, right: 20, bottom: 30, left: 150 };
+    const width = 600 - margin.left - margin.right;
+    const height = chartData.length * 20;
+
+    const x = d3.scaleLinear()
+        .domain([0, d3.max(chartData, d => d[1])])
+        .range([0, width]);
+
+    const y = d3.scaleBand()
+        .domain(chartData.map(d => d[0]))
+        .range([0, height])
+        .padding(0.1);
+
+    const g = svg.append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+
+    g.selectAll("rect")
+        .data(chartData)
+        .enter()
+        .append("rect")
+        .attr("y", d => y(d[0]))
+        .attr("width", d => x(d[1]))
+        .attr("height", y.bandwidth())
+        .attr("fill", "#69b3a2");
+
+    g.selectAll("text")
+        .data(chartData)
+        .enter()
+        .append("text")
+        .attr("x", d => x(d[1]) + 5)
+        .attr("y", d => y(d[0]) + y.bandwidth() / 2 + 4)
+        .text(d => d[1]);
+
+    svg.append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`)
+        .call(d3.axisLeft(y));
+}
 
 // Add basemap
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
