afcd33afce09deffd740877ec54e067cd0b6808b
NaNdhanee PM <madhavanm0809@gmail.com>
2025-07-22 14:44:36 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
-// Initialize the map
-const map = L.map('map', {
-  zoomControl: true,
-  attributionControl: false
-}).setView([7.8731, 80.7718], 7);
+
 
 // Basemap with NO labels and reduced opacity
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/light_nolabels/{z}/{x}/{y}{r}.png', {
+const map = L.map('map').setView([7.6500, 79.9000], 9);
+L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>',
   subdomains: 'abcd',
-  opacity: 0.8  // Set your desired opacity here
+  maxZoom: 19
 }).addTo(map);
 
 
