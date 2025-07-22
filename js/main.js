6d3f6c0dde4574bc39c79b38c6d1200ded6df305
NaNdhanee PM <madhavanm0809@gmail.com>
2025-07-22 20:09:19 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,12 +3,13 @@ const map = L.map('map', {
   attributionControl: false
 }).setView([7.8731, 80.7718], 7);
 
-// Dark basemap with NO labels
-const darkNoLabel = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
+// Carto Dark basemap with labels
+const cartoDark = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
   subdomains: 'abcd',
   maxZoom: 19,
   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
 }).addTo(map);
+
 
 
 
